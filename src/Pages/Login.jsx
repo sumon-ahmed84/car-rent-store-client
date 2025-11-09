8b91ddce9c0bbd95ef3b0681a8fac44305e19699
sumon-ahmed84 <sumonahmed275060@gmail.com>
2025-11-09 20:47:06 +0600

    create register page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,13 +1,13 @@
-import React, { useContext, useState } from "react";
+import React, { use, useState } from "react";
 import { toast } from "react-toastify";
 import { IoEyeOff } from "react-icons/io5";
 import { FaEye } from "react-icons/fa";
-import { FcGoogle } from "react-icons/fc";
 import { Link, useLocation, useNavigate } from "react-router";
 import { AuthContext } from "../context/AuthContext";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
-  const { login, googleLogin, forgotPassword } = useContext(AuthContext);
+  const { login, googleLogin } = use(AuthContext);
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [showPassword, setShowPassword] = useState(false);
 
@@ -15,8 +15,9 @@ const Login = () => {
   const location = useLocation();
   const from = location.state?.from?.pathname || "/";
 
-  const handleChange = (e) =>
+  const handleChange = (e) =>{
     setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -37,10 +38,10 @@ const Login = () => {
     }
   };
 
-  const handleForgotPassword = () => {
-    if (!formData.email) return alert("Enter your email first");
-    forgotPassword(formData.email);
-  };
+//   const handleForgotPassword = () => {
+//     if (!formData.email) return toast.error("Enter your email first");
+//     forgotPassword(formData.email);
+//   };
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-green-50">
@@ -79,7 +80,7 @@ const Login = () => {
           <div className="flex justify-between items-center">
             <button
               type="button"
-              onClick={handleForgotPassword}
+            //   onClick={handleForgotPassword}
               className="text-sm text-green-700 hover:underline"
             >
               Forgot Password?
@@ -96,33 +97,7 @@ const Login = () => {
           <p>Or login with Google</p>
           
           <button onClick={handleGoogle} className="btn w-full mt-2 bg-white text-black border-[#e5e5e5]">
-            <svg
-              aria-label="Google logo"
-              width="16"
-              height="16"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <g>
-                <path d="m0 0H512V512H0" fill="#fff"></path>
-                <path
-                  fill="#34a853"
-                  d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
-                ></path>
-                <path
-                  fill="#4285f4"
-                  d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
-                ></path>
-                <path
-                  fill="#fbbc02"
-                  d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
-                ></path>
-                <path
-                  fill="#ea4335"
-                  d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
-                ></path>
-              </g>
-            </svg>
+            <FcGoogle />
             Login with Google
           </button>
         </div>
